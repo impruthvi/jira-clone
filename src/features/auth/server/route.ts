f253a4e83d3a623c18ff9f581d40299160879459
impruthvi <pruthvirajput97@gmail.com>
2024-10-11 18:46:22 +0530

    Add useCurrent and useLogout hooks

--- src/features/auth/server/route.ts
+++ src/features/auth/server/route.ts
@@ -7,8 +7,14 @@ import { ID } from "node-appwrite";
 
 import { deleteCookie, setCookie } from "hono/cookie";
 import { AUTH_COOKIE } from "../constants";
+import { sessionMiddleware } from "@/lib/session-middleware";
 
 const app = new Hono()
+  .get("/current", sessionMiddleware, async (c) => {
+    const data = c.get("user");
+
+    return c.json({ data });
+  })
   .post("/login", zValidator("json", loginSchema), async (c) => {
     const { email, password } = c.req.valid("json");
 
@@ -43,8 +49,11 @@ const app = new Hono()
 
     return c.json({ success: true });
   })
-  .post("/logout", async (c) => {
+  .post("/logout", sessionMiddleware, async (c) => {
+    const account = c.get("account");
+
     deleteCookie(c, AUTH_COOKIE);
+    await account.deleteSession("current");
 
     return c.json({ success: true });
   });
