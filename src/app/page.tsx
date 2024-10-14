4e6946f7772fbe87445e59a059000430c0a5cc10
impruthvi <pruthvirajput97@gmail.com>
2024-10-14 11:41:31 +0530

    Redirect user to login page if user not logged in

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,28 +1,15 @@
-"use client";
-
-import { useCurrent } from "@/features/auth/api/use-current";
-import React, { useEffect } from "react";
-import { useRouter } from "next/navigation";
-import { Button } from "@/components/ui/button";
-import { useLogout } from "@/features/auth/api/use-logout";
+import { getCurrent } from "@/features/auth/actions";
+import { UserButton } from "@/features/auth/components/user-button";
+import { redirect } from "next/navigation";
 
 type Props = {};
 
-const Home = (props: Props) => {
-  const router = useRouter();
-  const { data, isLoading } = useCurrent();
-  const { mutate } = useLogout();
-  useEffect(() => {
-    if (!isLoading && !data) {
-      router.push("/sign-in");
-    }
-  }, [data]);
-  return (
-    <div>
-      Only authenticated users can see this page.
-      <Button onClick={() => mutate()}>Logout</Button>
-    </div>
-  );
+const Home = async (props: Props) => {
+  const user = await getCurrent();
+
+  if (!user) redirect("/sign-in");
+
+  return <UserButton />;
 };
 
 export default Home;
