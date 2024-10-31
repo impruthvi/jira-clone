056baca5cc0f1658d15629eca98657a0b7e31f86
impruthvi <pruthvirajput97@gmail.com>
2024-11-01 01:36:20 +0530

    Add workspace settings page and edit workspace form with validation

---
+++ src/app/(standalone)/workspaces/[workspaceId]/settings/page.tsx
@@ -0,0 +1,35 @@
+import { getCurrent } from "@/features/auth/actions";
+import { getWorkspace } from "@/features/workspaces/actions";
+import { EditWorkspaceForm } from "@/features/workspaces/components/edit-workspace-form";
+import { redirect } from "next/navigation";
+
+interface WorkspaceSettingsPageProps {
+  params: {
+    workspaceId: string;
+  };
+}
+
+const WorkspaceSettingsPage = async ({
+  params,
+}: WorkspaceSettingsPageProps) => {
+  const user = await getCurrent();
+  if (!user) redirect("/sign-in");
+
+  const initialValue = await getWorkspace({
+    workspaceId: params.workspaceId,
+  });
+
+  if (!initialValue) {
+    redirect(`/workspaces/${params.workspaceId}`);
+  }
+
+  console.log(`thiis is now`, initialValue);
+
+  return (
+    <div className="w-full lg:max-w-xl">
+      <EditWorkspaceForm initialValue={initialValue} />
+    </div>
+  );
+};
+
+export default WorkspaceSettingsPage;
