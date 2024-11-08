e105f8578bda402b69c991148d1f74dfd2d02d1d
impruthvi <pruthvirajput97@gmail.com>
2024-11-08 21:39:46 +0530

    Add join workspace functionality with invite code handling

---
+++ src/features/workspaces/components/join-workspace-form.tsx
@@ -0,0 +1,82 @@
+"use client";
+
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+
+import { DottedSeparator } from "@/components/dotted-separator";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+
+import { useJoinWorkspace } from "../api/use-join-workspace";
+import { useInviteCode } from "../hooks/use-invite-code";
+import { useWorkspaceId } from "../hooks/use-workspace-id";
+
+interface JoinWOrkspaceFormProps {
+  inviteValues: {
+    name: string;
+  };
+}
+
+export const JoinWorkspaceForm = ({
+  inviteValues: { name },
+}: JoinWOrkspaceFormProps) => {
+  const router = useRouter();
+  const inviteCode = useInviteCode();
+  const workspaceId = useWorkspaceId();
+  const { mutate, isPending } = useJoinWorkspace();
+
+  const onSubmit = () => {
+    mutate(
+      {
+        param: { workspaceId },
+        json: { code: inviteCode },
+      },
+      {
+        onSuccess: ({ data }) => {
+          router.push(`/workspaces/${data.$id}`);
+        },
+      }
+    );
+  };
+
+  return (
+    <Card className="w-full h-full border-none shadow-none">
+      <CardHeader className="p-7">
+        <CardTitle className="text-xl font-bold">Join Workspace</CardTitle>
+        <CardDescription>
+          You&apos;ve been invited to join <strong>{name}</strong> workspace
+        </CardDescription>
+      </CardHeader>
+      <div className="px-7">
+        <DottedSeparator />
+      </div>
+      <CardContent className="p-7">
+        <div className="flex flex-col lg:flex-row gap-2 items-center justify-between">
+          <Button
+            variant={"secondary"}
+            type="button"
+            asChild
+            className="w-full lg:w-fit"
+          >
+            <Link href="/">Cancel</Link>
+          </Button>
+          <Button
+            size={"lg"}
+            className="w-full lg:w-fit"
+            type="button"
+            onClick={onSubmit}
+            disabled={isPending}
+          >
+            Join Workspace
+          </Button>
+        </div>
+      </CardContent>
+    </Card>
+  );
+};
