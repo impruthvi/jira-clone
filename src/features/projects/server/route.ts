f7ec94da870769e35a22f2416282e2604e06a1cd
impruthvi <pruthvirajput97@gmail.com>
2024-11-10 10:24:08 +0530

    Add projects feature with API integration and UI components

---
+++ src/features/projects/server/route.ts
@@ -0,0 +1,40 @@
+import { z } from "zod";
+import { Hono } from "hono";
+import { Query } from "node-appwrite";
+import { zValidator } from "@hono/zod-validator";
+
+import { getMember } from "@/features/members/utils";
+
+import { DATABASE_ID, PROJECTS_ID } from "@/config";
+import { sessionMiddleware } from "@/lib/session-middleware";
+
+const app = new Hono().get(
+  "/",
+  sessionMiddleware,
+  zValidator("query", z.object({ workspaceId: z.string() })),
+  async (c) => {
+    const user = c.get("user");
+    const databases = c.get("databases");
+
+    const { workspaceId } = c.req.valid("query");
+
+    const member = await getMember({
+      databases,
+      workspaceId,
+      userId: user.$id,
+    });
+
+    if (!member) {
+      return c.json({ error: "Unauthorized" }, 401);
+    }
+
+    const projects = await databases.listDocuments(DATABASE_ID, PROJECTS_ID, [
+      Query.equal("workspaceId", workspaceId),
+      Query.orderDesc("$createdAt"),
+    ]);
+
+    return c.json({ data: projects });
+  }
+);
+
+export default app;
