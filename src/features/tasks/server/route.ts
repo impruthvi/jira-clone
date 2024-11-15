4a5e90c0884e86686a74a75ef38e75536cf6def3
impruthvi <pruthvirajput97@gmail.com>
2024-11-15 22:24:14 +0530

    Add delete task endpoint with authorization check and database integration

--- src/features/tasks/server/route.ts
+++ src/features/tasks/server/route.ts
@@ -15,6 +15,32 @@ import { createTaskSchema } from "../schemas";
 import { Task, TaskStatus } from "../types";
 
 const app = new Hono()
+  .delete("/:taskId", sessionMiddleware, async (c) => {
+    const user = c.get("user");
+    const databases = c.get("databases");
+    const { taskId } = c.req.param();
+    console.log("task id ", taskId);
+
+    const task = await databases.getDocument<Task>(
+      DATABASE_ID,
+      TASKS_ID,
+      taskId
+    );
+
+    const member = await getMember({
+      databases,
+      workspaceId: task.workspaceId,
+      userId: user.$id,
+    });
+
+    if (!member) {
+      return c.json({ error: "Unauthorized" }, 401);
+    }
+
+    await databases.deleteDocument(DATABASE_ID, TASKS_ID, task.$id);
+
+    return c.json({ data: { $id: task.$id } });
+  })
   .get(
     "/",
     sessionMiddleware,
