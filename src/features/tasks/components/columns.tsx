57c599ed64794c241e0c671ea72d46e4493ad866
impruthvi <pruthvirajput97@gmail.com>
2024-11-15 22:24:36 +0530

    Update task columns to use $id and add workspaceId to Task type

--- src/features/tasks/components/columns.tsx
+++ src/features/tasks/components/columns.tsx
@@ -131,7 +131,7 @@ export const columns: ColumnDef<Task>[] = [
   {
     id: "actions",
     cell: ({ row }) => {
-      const id = row.original.id;
+      const id = row.original.$id;
 
       const projectId = row.original.projectId;
 
