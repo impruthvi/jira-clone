b99a4f3ba72d753a445701c10d72f32ff7a3ec24
impruthvi <pruthvirajput97@gmail.com>
2024-11-17 12:13:20 +0530

    Add TaskOverview and OverviewProperty components for task details display

--- src/app/(dashboard)/workspaces/[workspaceId]/tasks/[taskId]/client.tsx
+++ src/app/(dashboard)/workspaces/[workspaceId]/tasks/[taskId]/client.tsx
@@ -1,11 +1,13 @@
 "use client";
 
-import { PageError } from "@/components/page-error";
-import { PageLoader } from "@/components/page-loader";
-
 import { useGetTask } from "@/features/tasks/api/use-get-task";
 import { useTaskId } from "@/features/tasks/hooks/use-task-id";
+import { TaskOverview } from "@/features/tasks/components/task-overview";
 import { TaskBreadcrumbs } from "@/features/tasks/components/task-breadcrumbs";
+
+import { PageError } from "@/components/page-error";
+import { PageLoader } from "@/components/page-loader";
+import { DottedSeparator } from "@/components/dotted-separator";
 
 export const TaskIdClient = () => {
   const taskId = useTaskId();
@@ -22,6 +24,10 @@ export const TaskIdClient = () => {
   return (
     <div className="flex flex-col">
       <TaskBreadcrumbs project={data.project} task={data} />
+      <DottedSeparator className="my-6" />
+      <div className="grid grid-cols-1 lg:grid-cols-2 gap-4">
+        <TaskOverview task={data} />
+      </div>
     </div>
   );
 };
