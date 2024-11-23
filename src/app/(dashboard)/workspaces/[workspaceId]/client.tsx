f52184463ece167111c46643f8785eb8554cad3c
impruthvi <pruthvirajput97@gmail.com>
2024-11-23 10:20:33 +0530

    Added Task listing on workspace home page

--- src/app/(dashboard)/workspaces/[workspaceId]/client.tsx
+++ src/app/(dashboard)/workspaces/[workspaceId]/client.tsx
@@ -1,6 +1,8 @@
 "use client";
 
-import { PlusIcon } from "lucide-react";
+import Link from "next/link";
+import { formatDistanceToNow } from "date-fns";
+import { CalendarIcon, PlusIcon } from "lucide-react";
 
 import { Task } from "@/features/tasks/types";
 import { useGetTasks } from "@/features/tasks/api/use-get-tasks";
@@ -15,6 +17,8 @@ import Analytics from "@/components/analytics";
 import { Button } from "@/components/ui/button";
 import { PageError } from "@/components/page-error";
 import { PageLoader } from "@/components/page-loader";
+import { Card, CardContent } from "@/components/ui/card";
+import { DottedSeparator } from "@/components/dotted-separator";
 
 export const WorkspaceIdClient = () => {
   const workspaceId = useWorkspaceId();
@@ -65,6 +69,7 @@ interface TaskListProps {
 }
 
 export const TaskList = ({ tasks, total }: TaskListProps) => {
+  const workspaceId = useWorkspaceId();
   const { open: createTask } = useCreateTaskModal();
   return (
     <div className="flex flex-col gap-y-4 col-span-1">
@@ -75,6 +80,36 @@ export const TaskList = ({ tasks, total }: TaskListProps) => {
             <PlusIcon className="size-4 text-neutral-400" />
           </Button>
         </div>
+        <DottedSeparator className="my-4" />
+        <ul className="flex flex-col gap-y-4">
+          {tasks.map((task) => (
+            <li key={task.$id}>
+              <Link href={`/workspaces/${workspaceId}/tasks/${task.$id}`}>
+                <Card className="shadow-none rounded-lg hover:opacity-75 transition">
+                  <CardContent className="p-4">
+                    <p className="text-lg font-medium truncate">{task.name}</p>
+                    <div className="flex items-center gap-x-2">
+                      <p>{task.project?.name}</p>
+                      <div className="size-1 rounded-full bg-neutral-300" />
+                      <div className="text-sm text-muted-foreground flex items-center">
+                        <CalendarIcon className="size-3 mr-1" />
+                        <span className="truncate">
+                          {formatDistanceToNow(new Date(task.dueDate))}
+                        </span>
+                      </div>
+                    </div>
+                  </CardContent>
+                </Card>
+              </Link>
+            </li>
+          ))}
+          <li className="text-sm text-muted-foreground text-center hidden first-of-type:block">
+            No Tasks Fount
+          </li>
+        </ul>
+        <Button variant={"muted"} className="mt-4 w-full" asChild>
+          <Link href={`workspaces/${workspaceId}/tasks`}>Show All</Link>
+        </Button>
       </div>
     </div>
   );
