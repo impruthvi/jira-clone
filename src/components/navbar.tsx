130aea4ee41ac5e05addf7da20cfbcd271b49a3b
impruthvi <pruthvirajput97@gmail.com>
2024-11-23 10:59:08 +0530

    Update Navbar component to display dynamic titles and descriptions based on the current pathname

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,14 +1,39 @@
+"use client";
+
+import { usePathname } from "next/navigation";
+
 import { UserButton } from "@/features/auth/components/user-button";
+
 import { MobileSidebar } from "./mobile-sidebar";
 
+const pathnameMap = {
+  tasks: {
+    title: "My Tasks",
+    description: "View all of your tasks here",
+  },
+  projects: {
+    title: "My Projects",
+    description: "View all tasks of your projects here",
+  },
+};
+
+const defaultMap = {
+  title: "Home",
+  description: "Monitor all your project and task",
+};
+
 export const Navbar = () => {
+  const pathname = usePathname();
+  const pathnameParts = pathname.split("/");
+  const pathnameKey = pathnameParts[3] as keyof typeof pathnameMap;
+
+  const { description, title } = pathnameMap[pathnameKey] || defaultMap;
+
   return (
     <nav className="pt-4 px-6 flex items-center justify-between">
       <div className="flex-col hidden lg:flex">
-        <h1 className="text-2xl font-semibold">Home</h1>
-        <p className="text-muted-foreground">
-          Monitor all your project and task
-        </p>
+        <h1 className="text-2xl font-semibold">{title}</h1>
+        <p className="text-muted-foreground">{description}</p>
       </div>
       <MobileSidebar />
       <UserButton />
