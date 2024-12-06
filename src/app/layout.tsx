817d1c9be772d3ac410328427f79d01bcb7a5da2
impruthvi <pruthvirajput97@gmail.com>
2024-12-06 10:01:50 +0530

    Add @vercel/analytics package and integrate it into the layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { Analytics } from '@vercel/analytics/next';
 
 import { cn } from "@/lib/utils";
 import { QueryProviders } from "@/components/query-provider";
@@ -28,6 +29,7 @@ export default function RootLayout({
         <QueryProviders>
           <Toaster />
           {children}
+          <Analytics />
         </QueryProviders>
       </body>
     </html>
